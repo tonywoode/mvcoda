5ae801608434174473f17beed54079f9e5ea1504
twoode <twoode@DTE0767.channel4.local>
2013-05-03 11:26:10 +0100

    added SLF4J basic jar (needed by xuggle) added xuggler examples - these work

---
+++ MVODA/src/xuggler/TranscodingExample.java
@@ -0,0 +1,38 @@
+package xuggler;
+
+import com.xuggle.mediatool.IMediaReader;
+import com.xuggle.mediatool.IMediaViewer;
+import com.xuggle.mediatool.IMediaWriter;
+import com.xuggle.mediatool.ToolFactory;
+
+public class TranscodingExample {
+
+    private static final String inputFilename = "C:/Users/Tony/Inputs/4kAvi/ColdplayFixYou.avi";
+    private static final String outputFilename = "C:/ColdplayFixYou.avi";
+
+    public static void main(String[] args) {
+
+        // create a media reader
+        IMediaReader mediaReader = 
+               ToolFactory.makeReader(inputFilename);
+        
+        // create a media writer
+        IMediaWriter mediaWriter = 
+               ToolFactory.makeWriter(outputFilename, mediaReader);
+
+        // add a writer to the reader, to create the output file
+        mediaReader.addListener(mediaWriter);
+        
+        // create a media viewer with stats enabled
+        IMediaViewer mediaViewer = ToolFactory.makeViewer(true);
+        
+        // add a viewer to the reader, to see the decoded media
+        mediaReader.addListener(mediaViewer);
+
+        // read and decode packets from the source file and
+        // and dispatch decoded audio and video to the writer
+        while (mediaReader.readPacket() == null) ;
+
+    }
+
+}
