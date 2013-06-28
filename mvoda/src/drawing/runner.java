55eb523f2a6dde08959ad6fa16d9df7c0f56fe93
Tony Woode <tonywoodecode@gmail.com>
2013-06-28 18:00:14 +0100

    managed to extract the number from the logo image sequence names. Rather long winded!

--- mvoda/src/drawing/runner.java
+++ mvoda/src/drawing/runner.java
@@ -4,6 +4,9 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
@@ -11,19 +14,83 @@ import javax.imageio.ImageIO;
 
 public class runner {
 
+
+	static String backFile = "../../../MVODAInputs/bru.png";
+	static String overlayFile = "../../../Repo/mvoda/mvoda/Theme/Pop/Logo/4MLogoFrames/4M68.png";
+	String outputFile = "../../../MVODAOutputs/CompositedOutput.png";
+
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-
-		String backFile = "../../../MVODAInputs/bru.png";
-		String overlayFile = "../../../Repo/mvoda/mvoda/Theme/Pop/Logo/4MLogoFrames/4M68.png";
-		String outputFile = "../../../MVODAOutputs/CompositedOutput.png";
+		
+		String dir = "Theme/Pop/Logo/4MLogoFrames";
+		File file = new File(dir);  
+		File[] files = file.listFiles();
+		String i = files[10].toString();
+				System.out.println(i);
+				/*Scanner in = new Scanner(i);
+				in.useDelimiter("");
+				while (in.hasNext())
+				{
+					char ch = in.next().charAt(0);
+					
+				}
+				int j = 0;
+				while (!Character.isDigit(i.charAt(j))) {
+					j++;
+				}
+				System.out.println(i.substring(15));*/
+		
+				String path = i;
+				String base = dir;
+				String relative = new File(base).toURI().relativize(new File(path).toURI()).getPath();
+				System.out.println(relative);
+				String reverse = new StringBuilder(relative).reverse().toString();
+				System.out.println(reverse);
+				int j = 0;
+				while (!Character.isDigit(reverse.charAt(j))) {
+					j++;
+				}
+				System.out.println(j);
+				String filenameRemoved = reverse.substring(j);
+				System.out.println(filenameRemoved);
+				int k = 0;
+				while (Character.isDigit(filenameRemoved.charAt(k))) {
+					k++;
+				}
+				String numberExtracted = filenameRemoved.substring(0,k);
+				System.out.println(numberExtracted);
+				String reverseBack = new StringBuilder(numberExtracted).reverse().toString();
+				System.out.println(reverseBack);
+				
+		/*
+				 File file = new File(dir);  
+				 File[] files = file.listFiles();				 
+				 Arrays.sort(files, new NumberedFileComparator());
+				 for (int fileInList = 0; fileInList < files.length; fileInList++)  
+				 {  
+				     System.out.println(files[fileInList].toString());  
+				 } 
+		
+	*/
+		
+		
+		
+		
+		
+		
+		
+		
 		//ImageCompositor overlayframes = new ImageCompositor(backFile, overlayFile, outputFile);
 		//overlayframes.overlay();
+		
+		//testOverlayImage();
+	}
 
 
+	static void testOverlayImage() throws IOException {
 		//great, now let's PASS you in the frames
 		Image bk = ImageIO.read(new File(backFile));
 		BufferedImage back = (BufferedImage) bk;
@@ -38,9 +105,10 @@ public class runner {
 		//hmm nothing wrong with the image viewer I just made, so why doesn't the media runner show me a video frame eh? See:
 		ShowImageInFrame gui = new ShowImageInFrame(composite);
 		//gui.createAndShowGui();
-
-
-
 	}
+
+	
+
+
 
 }
