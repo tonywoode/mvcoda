47a14ec5a77f8c32c1312e8bd1cc24815230b909
Tony Woode <tonywoodecode@gmail.com>
2013-07-16 14:06:53 +0100

    changed image compositor class to use fields rather than passing round the same variables endlessly. Now much more readable. Quite a big change to this class

--- mvoda/src/drawing/ImageCompositor.java
+++ mvoda/src/drawing/ImageCompositor.java
@@ -20,40 +20,35 @@ import lombok.Getter;
  */
 public class ImageCompositor {
 
-	//@Setter private BufferedImage image; //TODO: should these really be setters and not passed parameters?
-	//@Setter private BufferedImage overlayImage;
-	//private BufferedImage composite;
 	private int fileIndex;
 	private ArrayList<String> gfxFiles;
 	private GFXElement gfxElement;
+	
+	long inTime = 0;
+	long desiredDuration = 0;
+	long vidTimeStamp = 0;
+	long outTime = 0;
+	
+	BufferedImage videoFrame;
+	BufferedImage overlay;
+	
+	int x = 0;
+	int y = 0;
+	double newWidth = 0;
+	double newHeight = 0;
+	
 	@Getter boolean imOut = false;
 	private float alpha = 0f;
 	boolean fadeIt = false;
-	double newWidth = 0;
-	double newHeight = 0;
-
-
+	
 	/**
-	 * Takes a theme name and arranges to overlay the sequence of images set as logo TODO: we can have a choice of logos probably now
+	 * Takes a theme name and arranges to overlay the sequence of images set as logo
 	 * @param gfxElement the element to be composited
 	 */
 	public ImageCompositor(GFXElement gfxElement) {
 		this.gfxElement = gfxElement;
 		String dir = gfxElement.getDirectory();
 		gfxFiles = gfxElement.getOverlayFileNames(dir);
-	}
-
-	/*public ImageCompositor(GFXElement gfxElement, int num) {
-		this.gfxElement = gfxElement;
-		Numbers number = (Numbers) gfxElement;
-		number.setNum(num);
-		number.setDirectory(num)
-		String dir = number.getDirectory();
-		gfxFiles = number.getOverlayFileNames(dir);
-	}*/
-
-	public void resetFileUNC() {
-		fileIndex = 0;
 	}
 
 	/**
@@ -66,29 +61,29 @@ public class ImageCompositor {
 	 * @throws IOException
 	 */
 	public BufferedImage overlayNextImage(long vidTimeStamp, long inTime, long desiredDuration, BufferedImage videoFrame) throws IOException {
-		BufferedImage composite = overlayNextImage(vidTimeStamp, inTime, desiredDuration, videoFrame, gfxElement.getXOffsetSD(), gfxElement.getYOffsetSD());
-		return composite;
-	}
-
-	//TODO: you just made the splitting of these methods defunct as everything you CAN overlay DEFINTELY has x and y offsets in the class.....
-
-
-	public BufferedImage overlayNextImage(long vidTimeStamp, long inTime, long desiredDuration, BufferedImage videoFrame, int x, int y) throws IOException {
-
-		nextFileUNC(vidTimeStamp, inTime, desiredDuration);
+		x = gfxElement.getXOffsetSD();
+		y = gfxElement.getYOffsetSD();
+		this.vidTimeStamp = vidTimeStamp;
+		this.inTime = inTime;
+		this.desiredDuration = desiredDuration;
+		this.videoFrame = videoFrame;
+		outTime = inTime + desiredDuration;
+		
+		
+		
+		nextFileUNC();
 		String overlayFile = gfxFiles.get(fileIndex);
-		BufferedImage overlay = ImageIO.read(new File(overlayFile));
+		overlay = ImageIO.read(new File(overlayFile));
 		if (fadeIt) {
-			//BufferedImage composite = fadeImage(vidTimeStamp, inTime, desiredDuration, videoFrame, overlay, x, y);
-			BufferedImage composite = wipeImage(vidTimeStamp, inTime, desiredDuration, videoFrame, overlay, x, y);
+			//BufferedImage composite = fadeImage();
+			BufferedImage composite = wipeImage();
 			return composite;
 		}
 		else {
-			BufferedImage composite = overlayImage(videoFrame, overlay, x, y);
+			BufferedImage composite = overlayImage();
 			return composite;
 		}
 	}
-
 
 
 	/**
@@ -98,11 +93,11 @@ public class ImageCompositor {
 	 * @param inTime the time specified for the element to animate on
 	 * @param desiredDuration the total duration the element should last for
 	 */
-	public void nextFileUNC(long vidTimeStamp, long inTime, long desiredDuration){ //just set duration to zero to play for natural length
+	public void nextFileUNC() { //just set duration to zero to play for natural length
 		imOut = true;
-		long outTime = inTime + desiredDuration;
+		//long outTime = inTime + desiredDuration;
 		if (gfxFiles.size() == 1) { fadeIt = true; return; } //if theres just a static image rather than a sequence, return it, don't do the below
-		else if (gfxElement.getOutDuration() <= 0) { nextFileUNCForReverseOut(vidTimeStamp, inTime, outTime);} //if its a reverse out, go to that method
+		else if (gfxElement.getOutDuration() <= 0) { nextFileUNCForReverseOut();} //if its a reverse out, go to that method
 		else if (fileIndex < gfxFiles.size() -1 ) { //if we aren't at the last element frame
 			if (vidTimeStamp >= inTime) { //and if we are at the specified in time
 				if (fileIndex < gfxElement.getLastInFrame() ) { //and if we aren't at the half-way point of the element
@@ -128,10 +123,8 @@ public class ImageCompositor {
 		//return; 				//else we are before, after, or at the animation hold point, so don't animate...
 	}
 
-
-
-
-	public void nextFileUNCForReverseOut(long vidTimeStamp, long inTime, long outTime) {
+	
+	public void nextFileUNCForReverseOut() {
 		int outSpeedUp = 2; //factor by which we speed up the out. This is a common trick for reverse-out animations
 		if (fileIndex < gfxFiles.size() -1 && vidTimeStamp < outTime && vidTimeStamp >= inTime) { //if we arent at the last element frame or the outTime, but we are past the intime,
 			fileIndex++;} //animate
@@ -139,61 +132,48 @@ public class ImageCompositor {
 			fileIndex = fileIndex - outSpeedUp; //iterate backwards through the animation at the specified time factor
 			if (fileIndex > 0 && fileIndex < outSpeedUp) {fileIndex = 0; } //but we need the animation to end on blank frame zero irrespective of outSpeedUp factor
 		}
-		if (vidTimeStamp > inTime + gfxElement.getInDuration() + 500//TODO: repeating code!
+		if (vidTimeStamp > inTime + gfxElement.getInDuration() + 500//TODO: repeating code AND I SUSPECT ITS FIRING TWICE!
 				&& vidTimeStamp < outTime + (gfxElement.getInDuration() / 2 ) + 1000) { //nb: can't use out duration, use in divided by speedup factor
 			imOut = false; }
-		//return;
 	}
 
 
 
-	public BufferedImage overlayImage(BufferedImage image, BufferedImage overlayImage, int x, int y) throws IOException {
-		// create the new image, canvas size is the max. of both image sizes
-		//int w = image.getWidth();
-		//int h = image.getHeight();
-		//BufferedImage combined = new BufferedImage(w, h, BufferedImage.TYPE_3BYTE_BGR);
-
-		// paint both images, preserving the alpha channels
-		Graphics2D g = image.createGraphics();
-		//g.drawImage(image, 0, 0, null);
-		//if (fadeIt) { g.drawImage(fadeImage(image,overlayImage,x,y), 0,0, null); }
-		//overlayImage = fadeImage(overlayImage,x ,y );
-		g.drawImage(overlayImage, x, y, null);
-		//ImageIO.write(combined, "PNG", new File(outputFile));
-		return image;
+	public BufferedImage overlayImage() throws IOException {
+		//see history for some ways to make a new bufferedimage of correct type for xuggle and to output to file instead of passing back
+		Graphics2D g = videoFrame.createGraphics();
+		g.drawImage(overlay, x, y, null);
+		return videoFrame;
 	}
 
 
 
-	public BufferedImage fadeImage(long vidTimeStamp, long inTime, long desiredDuration, BufferedImage image, BufferedImage overlayImage, int x, int y) throws IOException {
-		//http://www.java2s.com/Code/Java/2D-Graphics-GUI/Fadeoutanimageimagegraduallygetmoretransparentuntilitiscompletelyinvisible.htm
+	public BufferedImage fadeImage() throws IOException {//http://www.java2s.com/Code/Java/2D-Graphics-GUI/Fadeoutanimageimagegraduallygetmoretransparentuntilitiscompletelyinvisible.htm
+		
 		float alphaFactor = 0.06f;
-		Graphics2D g2d = image.createGraphics();
-		long outTime = inTime + desiredDuration;
-		//alphaFactor = desiredDuration /   YES THIS IS THE WAY TO DO IT - ITS ZERO TO ONE: WORK OUT HOW LONG IT TAKES FOR EACH PERCENTAGE
+		Graphics2D g2d = videoFrame.createGraphics();
 
 		if ( vidTimeStamp >= inTime && vidTimeStamp <= outTime) {
 			g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
-			g2d.drawImage(overlayImage, x, y, null);
+			g2d.drawImage(overlay, x, y, null);
 			if  (alpha < 1.00f - alphaFactor ) {	alpha += alphaFactor; }
 		}
 		else if ( vidTimeStamp >= outTime ) { //TODO: doesn't fade out completely does it, knew it wouldn't - and these algorithms are a bit silly....
 			g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
-			g2d.drawImage(overlayImage, x, y, null);
+			g2d.drawImage(overlay, x, y, null);
 			if ( alpha - alphaFactor >= 0.00f ) { alpha -= alphaFactor; }
 			if ( alpha - alphaFactor < 0.0f ) { alpha = 0.0f; } //we really want it to fade off at the end
 		}
-			return image;
+			return videoFrame;
 		}
 	
 	
-	public BufferedImage wipeImage(long vidTimeStamp, long inTime, long desiredDuration,BufferedImage image, BufferedImage overlayImage, int x, int y) throws IOException {
+	public BufferedImage wipeImage() throws IOException {
 		
-		long outTime = inTime + desiredDuration;
 		if (vidTimeStamp > inTime && vidTimeStamp <= outTime ) {
 		double fadeTime = 50;
-		double width = overlayImage.getWidth();
-		double height = overlayImage.getHeight();
+		double width = overlay.getWidth();
+		double height = overlay.getHeight();
 			
 		double widthInc = width / fadeTime;
 		double heightInc = height / fadeTime;
@@ -208,22 +188,13 @@ public class ImageCompositor {
 		if (newWidth + widthInc <= width) { newWidth = newWidth + widthInc; }
 		if (newHeight + heightInc <= height) { newHeight = newHeight + heightInc; }
 		}
-		
-		/*System.out.println("Width is: " + overlayImage.getWidth()  );
-		System.out.println("Height is: " + overlayImage.getHeight() );
-		System.out.println("WidthFactor is: "+ widthInc);
-		System.out.println("HeightFactor is: "+ heightInc);
-		System.out.println("New Width is currently: " + newWidth  );
-		System.out.println("New Height is currently: " + newHeight  );*/
-		
-		
-		overlayImage = overlayImage.getSubimage(0, 0, (int) Math.ceil(newWidth), (int) Math.ceil(newHeight) );//TODO: read again then delete: http://stackoverflow.com/questions/2386064/how-do-i-crop-an-image-in-java
-		Graphics2D g = image.createGraphics();
-		
-		
-		g.drawImage(overlayImage, x, y, null);
+		/*System.out.println("Width is: " + overlayImage.getWidth()  );System.out.println("Height is: " + overlayImage.getHeight() );System.out.println("WidthFactor is: "+ widthInc);
+		System.out.println("HeightFactor is: "+ heightInc);System.out.println("New Width is currently: " + newWidth  );System.out.println("New Height is currently: " + newHeight  );*/
+		overlay = overlay.getSubimage(0, 0, (int) Math.ceil(newWidth), (int) Math.ceil(newHeight) );//TODO: read again then delete: http://stackoverflow.com/questions/2386064/how-do-i-crop-an-image-in-java
+		Graphics2D g = videoFrame.createGraphics();
+		g.drawImage(overlay, x, y, null);
 		}
-		return image;
+		return videoFrame;
 	}
 	
 
