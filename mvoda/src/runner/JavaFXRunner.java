d312fc9243a2cae1ea9a6760f63ff92663b2fe59
Tony Woode <tonywoodecode@gmail.com>
2013-08-15 17:43:27 +0100

    javafx added to project in tandem with installing e(fx)clipse to eclipse

---
+++ mvoda/src/runner/JavaFXRunner.java
@@ -0,0 +1,16 @@
+package runner;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFXRunner extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
