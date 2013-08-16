9bf5fbbea8cc70d876a2c72c921a4cc63b3cc5f0
Tony Woode <tonywoodecode@gmail.com>
2013-08-16 14:02:40 +0100

    demo gui now being loaded from fxml - this must be in the runner folder

--- mvoda/src/runner/JavaFXRunner.java
+++ mvoda/src/runner/JavaFXRunner.java
@@ -1,14 +1,31 @@
 package runner;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class JavaFXRunner extends Application {
-//here we go...
 	@Override
-	public void start(Stage primaryStage) {
-		
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("MVCODA");
+		Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+		stage.setScene(new Scene(root));
+		stage.show();
 	}
+	
+/*
+ * IF IT STOPS WORKING, TRY THIS:
+ * stage.setTitle("MVCODA");
+		URL location = getClass().getResource("GUI.fxml");
+		FXMLLoader fxmlloader = new FXMLLoader();
+		fxmlloader.setLocation(location);
+		fxmlloader.setBuilderFactory(new JavaFXBuilderFactory());
+		Parent root = (Parent) fxmlloader.load(location.openStream());
+		stage.setScene(new Scene(root));
+		stage.show();
+ */
 
 	public static void main(String[] args) {
 		launch(args);
