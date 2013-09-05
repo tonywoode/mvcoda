ba16a36baa678d71e0d960c9cd3618a3ce4a0d01
Tony Woode <tonywoodecode@gmail.com>
2013-09-05 16:13:34 +0100

    proving interesting trying to move "add new playlist entry" button....

--- mvoda/src/view/ViewController.java
+++ mvoda/src/view/ViewController.java
@@ -79,143 +79,8 @@ public class ViewController implements Initializable {
 	@FXML ListView<PlaylistEntry> playlistView;
 
 	private MoveButtons moveButtons;
-
-
-	public void sendPlaylistNodesToScreen(Playlist videos) {
-		for (PlaylistEntry playlistEntry : videos.getPlaylistEntries())
-			playlistView.getItems().add(playlistEntry);
-	}
-
-
-	@FXML void loadPlaylist(ActionEvent e) {
-
-		final FileChooser fileChooser = new FileChooser();
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
-		fileChooser.getExtensionFilters().add(extFilter);
-		File file = fileChooser.showOpenDialog(stage);
-		Playlist playlistTemp = new Playlist("hi");
-
-		playlistObservable.clear(); //TDO: now we need to make sure we clear all 3 of the playlists! 
-		playlist.resetArray(playlistObservable);
-
-		//System.out.println("in the playlist AT POINT 1 is /n" + playlist);
-		if (file != null) {
-			//try { 
-			XMLSerialisable playlistAsSerialisable = XMLReader.readPlyalistXML(file.toPath());	
-			playlistTemp = (Playlist) playlistAsSerialisable;
-
-		}
-
-
-		//TODO why do I need to instantiate the videos or else javafx will crash out the JVM, when appending to a pre-existing playlist will not make that happen
-		for (int i = 0; i < playlistTemp.getPlaylistEntries().size(); i++) {
-			PlaylistEntry entry = new PlaylistEntry( new MusicVideoXuggle( 
-					playlistTemp.getPlaylistEntries().get( i ).getFileUNC() ),
-					playlistTemp.getPlaylistEntries().get( i ).getTrackName(), 
-					playlistTemp.getPlaylistEntries().get( i ).getArtistName()
-					);
-			entry.setPositionInPlaylist(i + 1); //set the playlist entry number while we have a loop! may be a problem later.....
-			playlist.setNextEntry(entry);
-		}
-		playlist.setThemeName(playlistTemp.getThemeName()); //we need to set the actual playlist to the temp playlists theme name. Outside of loop as not entry specific
-
-		/*videos.setNextEntry(new PlaylistEntry(new MusicVideoXuggle(fileUNC), "Track 1", "Artist 1"));
-						videos.setNextEntry(new PlaylistEntry(new MusicVideoXuggle(fileUNC2), "Track 2", "Artist 2"));
-		 */
-		String themename = playlist.getThemeName();
-		playlistObservable = playlistView.getItems();
-		//System.out.println("in the playlist at point 2 is /n" + playlist);
-		sendPlaylistNodesToScreen(playlist);
-		if	( themeSelectBox.getItems().contains(playlist.getThemeName() ) ) { //if the theme name is actually one of our themes	
-			ObservableList<String> themeBoxItems = themeSelectBox.getItems(); //turn the theme box's list into an iterable list
-				for (String itemName : themeBoxItems) { //iterate through it
-					if (themename.equals(itemName)) //until we match the text strings
-					{
-						//themeSelectBox.getSelectionModel().clearSelection();
-						//themeSelectBox.getSelectionModel().select(themename);
-						themeSelectBox.setValue(themename); //and set that theme name as the active one in both the box and the list the box is generated from
-					}
-				}
-						
-		}
-	}
-
-
-	@FXML void savePlaylist(ActionEvent e) {
-		playlist.setThemeName(themeSelectBox.getSelectionModel().getSelectedItem().toString()); //Set theme name in the playlist xml
-		playlist.resetArray(playlistObservable);
-		setNumbersInPlaylist();
-		XMLSerialisable xmlSerialisable = playlist;
-		final FileChooser fileChooser = new FileChooser();
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
-		fileChooser.getExtensionFilters().add(extFilter);
-		File file = fileChooser.showSaveDialog(stage);
-		String fileAsString = "";
-		if(!file.getName().contains(".xml")) { 	fileAsString = file.toString() + ".xml"; } //this check helps if the file is already existing as .xml
-		else { fileAsString = file.toString(); } //else we will get "x.xml.xml"
-
-		Path path = Paths.get(fileAsString);
-
-		//Path pathPlusExt = Path(fileAsString);
-		XMLWriter.writePlaylistXML(true, path, xmlSerialisable);
-
-	}
 	
-	private void setNumbersInPlaylist() {
-		for (int t = 0; t < playlist.getPlaylistEntries().size(); t++) {
-			playlist.getPlaylistEntries().get(t).setPositionInPlaylist(t + 1); //set the playlist positions in the playlist to something sensible
-		}
-	}
-
-
-	@FXML void newPlaylist(ActionEvent e) { makeAPlaylist(); }
-
-
-	@FXML void render(ActionEvent e) {
-		System.out.println(playlist.toString());
-		playlist.resetArray(playlistObservable);
-		setNumbersInPlaylist();
-
-		for ( int i=0;i < playlist.getPlaylistEntries().size(); i++ ) {
-			System.out.println("At postion: " + (i + 1) + " We have " + playlist.getPlaylistEntries().get(i).getFileUNC() );
-		}
-
-		//mock the theme
-		String themeName = themeSelectBox.getSelectionModel().getSelectedItem().toString();
-
-		Path rootDir = Paths.get("Theme");
-		Path themeDir = Paths.get(rootDir.toString(),themeName);
-		XMLSerialisable themeAsSerialisable = XMLReader.readXML(themeDir, themeName);
-		Theme theme = (Theme) themeAsSerialisable;
-		theme.setIndex(themeSelectBox.getSelectionModel().getSelectedIndex()); //TODO; the lines above is effectively a new so any index setting before this has no effect
-		Path properDir = Paths.get( Theme.getRootDir().toString(), theme.getItemName() );
-
-		//draw onto video
-
-		//first we must ask where you want to save with a dialog
-		final FileChooser fileChooser = new FileChooser();
-		String filetype = playlist.getNextEntry(0).getVideo().getFiletype();
-		//TODO: problem is we need to get the FIRST files' filetype....is there a better way of encapsulating this its not obvious it go through about 3 classes...
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(filetype + " files (*" + filetype + ")", "*" + filetype);
-		fileChooser.getExtensionFilters().add(extFilter);
-		File file = fileChooser.showSaveDialog(stage);
-		String outFileUNC = "";
-		if(!file.getName().endsWith( filetype ) ) { 	outFileUNC = file.toString() + filetype; } //this check helps if the file is already existing
-		else { outFileUNC = file.toString(); } //else we will get "x.filetype.filetype //TODO: same code as in save playlist button
-
-		//String outFileUNC = file.toString() + filetype; //append filetype to the UNC the user chooses
-		if( file != null ) { Encoder draw = new EncoderXuggle(playlist, theme, outFileUNC); }
-
-		DecodeAndPlayAudioAndVideo player = new DecodeAndPlayAudioAndVideo(outFileUNC);
-	}
-
-	/*@FXML void playlistEntryEntered(ActionEvent e) {
-		String name = ""; // get from textbox
-		viewListener.onNewTrackAvailable(name);
-	}*/
-
-
-
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		//themeSelectBox.add("Hello");
@@ -239,7 +104,7 @@ public class ViewController implements Initializable {
 
 		System.out.println(themename);
 		themeSelectBox.setItems(themename);
-		moveButtons = new MoveButtons(playlistView); //instantiate the move buttons (we need to pass them a playlist)
+		moveButtons = new MoveButtons(playlistView, playlistObservable); //instantiate the move buttons (we need to pass them a playlist)
 
 		playlistView.setCellFactory(new Callback<ListView<PlaylistEntry>, ListCell<PlaylistEntry>>() {
 			@Override public ListCell<PlaylistEntry> call(ListView<PlaylistEntry> list) {
@@ -296,6 +161,186 @@ public class ViewController implements Initializable {
 		});
 	}
 
+
+	public void sendPlaylistNodesToScreen(Playlist videos) {
+		for (PlaylistEntry playlistEntry : videos.getPlaylistEntries())
+			playlistView.getItems().add(playlistEntry);
+	}
+
+
+	@FXML void loadPlaylist(ActionEvent e) {
+
+		final FileChooser fileChooser = new FileChooser();
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
+		fileChooser.getExtensionFilters().add(extFilter);
+		File file = fileChooser.showOpenDialog(stage);
+		Playlist playlistTemp = new Playlist("hi");
+
+		playlistObservable.clear(); //TDO: now we need to make sure we clear all 3 of the playlists! 
+		playlist.resetArray(playlistObservable);
+
+		//System.out.println("in the playlist AT POINT 1 is /n" + playlist);
+		if (file != null) {
+			//try { 
+			XMLSerialisable playlistAsSerialisable = XMLReader.readPlyalistXML(file.toPath());	
+			playlistTemp = (Playlist) playlistAsSerialisable;
+
+		}
+
+		//TODO why do I need to instantiate the videos or else javafx will crash out the JVM, when appending to a pre-existing playlist will not make that happen
+		for (int i = 0; i < playlistTemp.getPlaylistEntries().size(); i++) {
+			PlaylistEntry entry = new PlaylistEntry( new MusicVideoXuggle( 
+					playlistTemp.getPlaylistEntries().get( i ).getFileUNC() ),
+					playlistTemp.getPlaylistEntries().get( i ).getTrackName(), 
+					playlistTemp.getPlaylistEntries().get( i ).getArtistName()
+					);
+			entry.setPositionInPlaylist(i + 1); //set the playlist entry number while we have a loop! may be a problem later.....
+			playlist.setNextEntry(entry);
+		}
+		playlist.setThemeName(playlistTemp.getThemeName()); //we need to set the actual playlist to the temp playlists theme name. Outside of loop as not entry specific
+
+		/*videos.setNextEntry(new PlaylistEntry(new MusicVideoXuggle(fileUNC), "Track 1", "Artist 1"));
+						videos.setNextEntry(new PlaylistEntry(new MusicVideoXuggle(fileUNC2), "Track 2", "Artist 2"));
+		 */
+		String themename = playlist.getThemeName();
+		playlistObservable = playlistView.getItems();
+		//System.out.println("in the playlist at point 2 is /n" + playlist);
+		sendPlaylistNodesToScreen(playlist);
+		if	( themeSelectBox.getItems().contains(playlist.getThemeName() ) ) { //if the theme name is actually one of our themes	
+			ObservableList<String> themeBoxItems = themeSelectBox.getItems(); //turn the theme box's list into an iterable list
+				for (String itemName : themeBoxItems) { //iterate through it
+					if (themename.equals(itemName)) //until we match the text strings
+					{
+						//themeSelectBox.getSelectionModel().clearSelection();
+						//themeSelectBox.getSelectionModel().select(themename);
+						themeSelectBox.setValue(themename); //and set that theme name as the active one in both the box and the list the box is generated from
+					}
+				}
+						
+		}
+	}
+
+
+	@FXML void savePlaylist(ActionEvent e) {
+		
+		playlist.resetArray(playlistObservable);
+		playlist.setThemeName(themeSelectBox.getSelectionModel().getSelectedItem().toString()); //Set theme name in the playlist xml
+		setNumbersInPlaylist();
+		XMLSerialisable xmlSerialisable = playlist;
+		final FileChooser fileChooser = new FileChooser();
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
+		fileChooser.getExtensionFilters().add(extFilter);
+		File file = fileChooser.showSaveDialog(stage);
+		String fileAsString = "";
+		if(!file.getName().contains(".xml")) { 	fileAsString = file.toString() + ".xml"; } //this check helps if the file is already existing as .xml
+		else { fileAsString = file.toString(); } //else we will get "x.xml.xml"
+
+		Path path = Paths.get(fileAsString);
+
+		//Path pathPlusExt = Path(fileAsString);
+		XMLWriter.writePlaylistXML(true, path, xmlSerialisable);
+
+	}
+	
+	
+	public void addPlaylistEntry(ActionEvent e) throws IOException { //TOD: loading a music video exception please
+		PlaylistEntry entry = moveButtons.addPlaylistEntry(e, stage);
+		playlistObservable.add(entry); //TODO we cannot pass the observable list outside of the view controller, so we return a playlist entry here
+		}
+
+		
+	
+	
+	public void deletePlaylistEntry(ActionEvent e) { //https://gist.github.com/jewelsea/5559262
+		moveButtons.deletePlaylistEntry(e);
+	}
+
+
+	public void moveUp(ActionEvent e) {
+		//MoveButtons moveUpButton = new MoveButtons(playlistView);
+		//moveUpButton.moveUp(e);	
+		moveButtons.moveUp(e);
+	}
+
+	public void moveDown(ActionEvent e) {
+		
+		//Dialog dialog = new Dialog();
+		//Dialog.dialogBox(stage, "ehat");//, new Scene());
+			
+		try {
+			moveButtons.moveDown(e);
+		} catch (IndexOutOfBoundsException error) {
+			// TODO Auto-generated catch block
+			//Dialog.dialogBox(stage, "No playlist loaded. Please first create or load a playlist");//, new Scene());
+			error.printStackTrace();
+		}
+	}
+	
+		
+	
+	private void setNumbersInPlaylist() {
+		for (int t = 0; t < playlist.getPlaylistEntries().size(); t++) {
+			playlist.getPlaylistEntries().get(t).setPositionInPlaylist(t + 1); //set the playlist positions in the playlist to something sensible
+		}
+	}
+	
+	
+	
+
+	@FXML void newPlaylist(ActionEvent e) { makeAPlaylist(); }
+	
+	
+	
+	
+
+
+	@FXML void render(ActionEvent e) {
+		System.out.println(playlist.toString());
+		playlist.resetArray(playlistObservable);
+		setNumbersInPlaylist();
+
+		for ( int i=0;i < playlist.getPlaylistEntries().size(); i++ ) {
+			System.out.println("At postion: " + (i + 1) + " We have " + playlist.getPlaylistEntries().get(i).getFileUNC() );
+		}
+
+		//mock the theme
+		String themeName = themeSelectBox.getSelectionModel().getSelectedItem().toString();
+
+		Path rootDir = Paths.get("Theme");
+		Path themeDir = Paths.get(rootDir.toString(),themeName);
+		XMLSerialisable themeAsSerialisable = XMLReader.readXML(themeDir, themeName);
+		Theme theme = (Theme) themeAsSerialisable;
+		theme.setIndex(themeSelectBox.getSelectionModel().getSelectedIndex()); //TODO; the lines above is effectively a new so any index setting before this has no effect
+		Path properDir = Paths.get( Theme.getRootDir().toString(), theme.getItemName() );
+
+		//draw onto video
+
+		//first we must ask where you want to save with a dialog
+		final FileChooser fileChooser = new FileChooser();
+		String filetype = playlist.getNextEntry(0).getVideo().getFiletype();
+		//TODO: problem is we need to get the FIRST files' filetype....is there a better way of encapsulating this its not obvious it go through about 3 classes...
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(filetype + " files (*" + filetype + ")", "*" + filetype);
+		fileChooser.getExtensionFilters().add(extFilter);
+		File file = fileChooser.showSaveDialog(stage);
+		String outFileUNC = "";
+		if(!file.getName().endsWith( filetype ) ) { 	outFileUNC = file.toString() + filetype; } //this check helps if the file is already existing
+		else { outFileUNC = file.toString(); } //else we will get "x.filetype.filetype //TODO: same code as in save playlist button
+
+		//String outFileUNC = file.toString() + filetype; //append filetype to the UNC the user chooses
+		if( file != null ) { Encoder draw = new EncoderXuggle(playlist, theme, outFileUNC); }
+
+		DecodeAndPlayAudioAndVideo player = new DecodeAndPlayAudioAndVideo(outFileUNC);
+	}
+
+	/*@FXML void playlistEntryEntered(ActionEvent e) {
+		String name = ""; // get from textbox
+		viewListener.onNewTrackAvailable(name);
+	}*/
+
+
+
+	
+
 	/**
 	 * Makes an array of file unc paths to ten videos, then makes a new playlist, turns the UNC's into playlist entry music vids and adds them to the playlist,
 	 */
@@ -327,105 +372,9 @@ public class ViewController implements Initializable {
 		playlistObservable = playlistView.getItems();
 	}
 
-	public void addPlaylistEntry(ActionEvent e) throws IOException { //TOD: loading a music video exception please
-		final FileChooser fileChooser = new FileChooser();
-		//FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
-		//fileChooser.getExtensionFilters().add(extFilter);
-		File file = fileChooser.showOpenDialog(stage);
-		if (file != null) {
-			String fileUNC = file.getAbsolutePath();
-			MusicVideo vid = new MusicVideoXuggle(fileUNC);
-			PlaylistEntry entry = new PlaylistEntry( vid, "Track" + (playlist.getSize() + 1), "Artist" + (playlist.getSize() + 1) );
-			entry.setPositionInPlaylist(playlist.getSize() + 1);//no point in doing this really
-			playlist.setNextEntry(entry);
-			playlistObservable = playlistView.getItems();
-			playlistObservable.add(entry);
-			//sendPlaylistNodesToScreen(videos);	
-		}
-
-	}
-
-	public void deletePlaylistEntry(ActionEvent e) { //https://gist.github.com/jewelsea/5559262
-		PlaylistEntry toDelete = playlistView.getSelectionModel().getSelectedItem();
-		int indexOfItemToDelete = playlistView.getSelectionModel().getSelectedIndex();
-		//ObservableList<PlaylistEntry> playlistObservable = playlistView.getItems();
-		//can i commit
-		playlistView.getItems().remove(indexOfItemToDelete);
-	}
+	
 
 
-	public void moveUp(ActionEvent e) {
-		//MoveButtons moveUpButton = new MoveButtons(playlistView);
-		//moveUpButton.moveUp(e);	
-		moveButtons.moveUp(e);
-	}
-
-	public void moveDown(ActionEvent e) {
-		
-		//Dialog dialog = new Dialog();
-		//Dialog.dialogBox(stage, "ehat");//, new Scene());
-			
-		try {
-			moveButtons.moveDown(e);
-		} catch (IndexOutOfBoundsException error) {
-			// TODO Auto-generated catch block
-			//Dialog.dialogBox(stage, "No playlist loaded. Please first create or load a playlist");//, new Scene());
-			error.printStackTrace();
-		}
-	}
-
-
-
-
-
-	/*public void moveUp(ActionEvent e) {
-		int indexOfItemToMove = playlistView.getSelectionModel().getSelectedIndex();
-
-		if (indexOfItemToMove < 0) return; //don't attempt to move the top item
-
-		PlaylistEntry temp = playlistView.getSelectionModel().getSelectedItem(); //temp entry holds the entry we want to move
-		playlistView.getItems().set(indexOfItemToMove, playlistView.getItems().get(indexOfItemToMove - 1)); //set replaces the item: so move item below to selected index
-		playlistView.getItems().set(indexOfItemToMove - 1, temp); //now move 
-
-		PlaylistEntry movingUp = playlistView.getSelectionModel().getSelectedItem();
-		PlaylistEntry movingDown = playlistView.getItems().get(indexOfItemToMove - 1);
-
-		movingUp.setPositionInPlaylist(indexOfItemToMove);
-		movingDown.setPositionInPlaylist(indexOfItemToMove + 1);
-
-		System.out.println("Moving Up: " + movingUp.getPositionInPlaylist() + "; " + movingUp.getFileUNC());
-		System.out.println("Moving Down: " + movingDown.getPositionInPlaylist() + "; " + movingDown.getFileUNC());
-
-			forceListRefreshOn(playlistView);
-
-	playlistView.getSelectionModel().clearAndSelect(indexOfItemToMove - 1);	
-	playlistView.getFocusModel().focus(indexOfItemToMove - 1);	
-	playlistView.requestLayout();
-
-
-	}
-	 */
-	/*public void moveDown(ActionEvent e) {
-		int indexOfItemToMove = playlistView.getSelectionModel().getSelectedIndex();
-		int lastIndex = playlistView.getItems().size() -1;
-
-		if (indexOfItemToMove > lastIndex) return; //TODO: still causing exception is it because i'm only catching the viewbox's error condition not the lists?
-
-		PlaylistEntry temp = playlistView.getSelectionModel().getSelectedItem();
-		playlistView.getItems().set(indexOfItemToMove, playlistView.getItems().get(indexOfItemToMove + 1));
-		playlistView.getItems().set(indexOfItemToMove + 1, temp);
-
-		PlaylistEntry movingDown = playlistView.getSelectionModel().getSelectedItem();
-		PlaylistEntry movingUp = playlistView.getItems().get(indexOfItemToMove + 1);
-
-		movingDown.setPositionInPlaylist(indexOfItemToMove);
-		movingUp.setPositionInPlaylist(indexOfItemToMove);
-
-		System.out.println("Moving Down: " + movingDown.getPositionInPlaylist() + "; " + movingDown.getFileUNC());
-		System.out.println("Moving Up: " + movingUp.getPositionInPlaylist() + "; " + movingUp.getFileUNC());
-	}
-
-	 */
 
 	// adapted from: http://stackoverflow.com/questions/16880115/javafx-2-2-how-to-force-a-redraw-update-of-a-listview
 	private void forceListRefreshOn(ListView lsv) {
--- mvoda/src/view/buttons/MoveButtons.java
+++ mvoda/src/view/buttons/MoveButtons.java
@@ -1,10 +1,18 @@
 package view.buttons;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import media.MusicVideo;
+import media.xuggle.MusicVideoXuggle;
+
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import playlist.PlaylistEntry;
 import themes.GFXElement;
 
@@ -13,26 +21,56 @@ import themes.GFXElement;
 public class MoveButtons {
 	
 	ListView<PlaylistEntry> playlistView;
-	
-	//private final static Logger LOGGER = Logger.getLogger(MoveButtons.class.getName());
+	private ObservableList<PlaylistEntry> playlistObservable;
+	//private Stage stage;
 	private final static Logger LOGGER = Logger.getLogger(MoveButtons.class.getName()); //setup a logger for this class
 	 
 	
-	public MoveButtons(ListView<PlaylistEntry> playlistView) {
+	public MoveButtons(ListView<PlaylistEntry> playlistView, ObservableList<PlaylistEntry> playlistObservable) {
 		this.playlistView = playlistView;
-		LOGGER.setLevel(Level.ALL); //set the level of the logger
+		this.playlistObservable = playlistObservable;
+		//this.stage = stage;
+								LOGGER.setLevel(Level.ALL); //set the level of the logger
 	}
 	
-	public void moveUp(ActionEvent e) {
-		
-		
-		int indexOfItemToMove = playlistView.getSelectionModel().getSelectedIndex();
+	
+	public PlaylistEntry addPlaylistEntry(ActionEvent e, Stage stage) throws IOException { //TOD: loading a music video exception please
+		final FileChooser fileChooser = new FileChooser();
+		//FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
+		//fileChooser.getExtensionFilters().add(extFilter);
+		File file = fileChooser.showOpenDialog(stage);
+		//if (file != null) {
+			String fileUNC = file.getAbsolutePath();
+			MusicVideo vid = new MusicVideoXuggle(fileUNC);
+			PlaylistEntry entry = new PlaylistEntry( vid, "Track" + (playlistObservable.size() + 1), "Artist" + (playlistObservable.size() + 1) );
+			entry.setPositionInPlaylist(playlistObservable.size() + 1);//no point in doing this really
+			//playlist.setNextEntry(entry);
+			playlistObservable = playlistView.getItems();
+			playlistObservable.add(entry);
+			
+		//}
+		return entry;
+	}
+	
+	
+	public void deletePlaylistEntry(ActionEvent e) { //https://gist.github.com/jewelsea/5559262
+		//PlaylistEntry toDelete = playlistView.getSelectionModel().getSelectedItem();
+		int indexOfItemToDelete = playlistView.getSelectionModel().getSelectedIndex();
+		//ObservableList<PlaylistEntry> playlistObservable = playlistView.getItems();
 
+		int indexSize = playlistView.getItems().size();
+		//TODO still doesn't solve the issue where you lose focus and stop being able to delete after 2 items - yes 2 ITEMS!
+		if (indexOfItemToDelete >= 0 && indexOfItemToDelete < indexSize ) { playlistView.getItems().remove(indexOfItemToDelete); }
+	}
+		
+	
+	public void moveUp(ActionEvent e) {
+		int indexOfItemToMove = playlistView.getSelectionModel().getSelectedIndex();
 		if (indexOfItemToMove <= 0) { return; } //don't attempt to move the top item, or anything in an empty list
 
-		PlaylistEntry temp = playlistView.getSelectionModel().getSelectedItem(); //temp entry holds the entry we want to move
+		PlaylistEntry temp = playlistView.getSelectionModel().getSelectedItem(); //the entry we want to move
 		playlistView.getItems().set(indexOfItemToMove, playlistView.getItems().get(indexOfItemToMove - 1)); //set replaces the item: so move item below to selected index
-		playlistView.getItems().set(indexOfItemToMove - 1, temp); //now move 
+		playlistView.getItems().set(indexOfItemToMove - 1, temp); //now move original
 
 		PlaylistEntry moveDown = playlistView.getSelectionModel().getSelectedItem();
 		PlaylistEntry moveUp = playlistView.getItems().get(indexOfItemToMove - 1);
@@ -40,24 +78,14 @@ public class MoveButtons {
 		moveDown.setPositionInPlaylist(indexOfItemToMove);
 		moveUp.setPositionInPlaylist(indexOfItemToMove + 1);
 
-						LOGGER.info("Moving Up: " + moveUp.getPositionInPlaylist() + "; " + moveUp.getFileUNC());
-						LOGGER.info("Moving Down: " + moveDown.getPositionInPlaylist() + "; " + moveDown.getFileUNC());
-		
-
-		/*	forceListRefreshOn(playlistView);
-
-	playlistView.getSelectionModel().clearAndSelect(indexOfItemToMove - 1);	
-	playlistView.getFocusModel().focus(indexOfItemToMove - 1);	
-	playlistView.requestLayout();
-		 */	
-
+								LOGGER.info("Moving Up: " + moveUp.getPositionInPlaylist() + "; " + moveUp.getFileUNC());
+								LOGGER.info("Moving Down: " + moveDown.getPositionInPlaylist() + "; " + moveDown.getFileUNC());
 	}
 	
 	
 	public void moveDown(ActionEvent e) {
 		int indexOfItemToMove = playlistView.getSelectionModel().getSelectedIndex();
 		int lastIndex = playlistView.getItems().size() -1;
-
 		if (indexOfItemToMove == lastIndex) { return; } //JavaFX's view array returns -1 for empty list, hence we catch both moving off end of list AND empty list
 
 		PlaylistEntry temp = playlistView.getSelectionModel().getSelectedItem();
@@ -71,9 +99,10 @@ public class MoveButtons {
 		moveDown.setPositionInPlaylist(indexOfItemToMove);
 
 		
-						LOGGER.info("Moving Down: " + moveDown.getPositionInPlaylist() + "; " + moveDown.getFileUNC());
-						LOGGER.info("Moving Up: " + moveUp.getPositionInPlaylist() + "; " + moveUp.getFileUNC());
+								LOGGER.info("Moving Down: " + moveDown.getPositionInPlaylist() + "; " + moveDown.getFileUNC());
+								LOGGER.info("Moving Up: " + moveUp.getPositionInPlaylist() + "; " + moveUp.getFileUNC());
 	}
+	
 	
 
 }
