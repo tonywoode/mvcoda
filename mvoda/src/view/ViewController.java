a7fbf999a6e5bcd41125a43ec6d9ce4b87ab1eee
Tony Woode <tonywoodecode@gmail.com>
2013-09-06 09:06:13 +0100

    testing commit from stream

--- mvoda/src/view/ViewController.java
+++ mvoda/src/view/ViewController.java
@@ -269,7 +269,7 @@ public class ViewController implements Initializable {
 		else { fileAsString = file.toString(); } //else we will get "x.xml.xml"
 
 		Path path = Paths.get(fileAsString);
-//commit check //comit check again
+//commit check //comit check again //now from stream
 		//Path pathPlusExt = Path(fileAsString);
 		XMLWriter.writePlaylistXML(true, path, xmlSerialisable);
 
