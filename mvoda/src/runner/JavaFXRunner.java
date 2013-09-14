4e7e7507068f582bdff7bed1aaa79fb9e40f4492
Tony Woode <tonywoodecode@gmail.com>
2013-09-14 02:31:14 +0100

    new branch

--- mvoda/src/runner/JavaFXRunner.java
+++ mvoda/src/runner/JavaFXRunner.java
@@ -23,7 +23,7 @@ public class JavaFXRunner extends Application {
 	public void start(Stage stage) throws Exception {
 		
 		/**
-		 * Configure logging in this class also in runner package
+		 * Configure logging in this class, the logging controller is also in runner package
 		 */
 		HandleLoggers.allLoggers();
 		
